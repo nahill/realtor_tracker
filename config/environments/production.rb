8f390b2e90901fac2d864a568636de7207f02340
nahill <nahill89@gmail.com>
2015-08-15 01:24:22 +0000

    Fixed Bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Use Rack-WWWhisper for authenication in production
   config.middleware.insert 0, "Rack::WWWhisper"
 end
