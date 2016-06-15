c92cff349615aca2834e0da6b36786589db2fa48
nealhdotdev <nealh.dev@gmail.com>
2016-06-15 03:54:36 +0000

    precompiling assests for rails_admin

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
+
 module RealtorTracker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
