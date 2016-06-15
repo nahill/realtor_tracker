f92e962e5e7a83cf5a0638c382a4e17b7d8c07f8
nealhdotdev <nealh.dev@gmail.com>
2016-06-15 04:31:51 +0000

    changes to config file

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module RealtorTracker
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
   end
 end
