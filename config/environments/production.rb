7bae63dcd1e2b1ae9dac059b44adc73fa3124cf9
nealhdotdev <nealh.dev@gmail.com>
2016-07-25 20:48:03 +0000

    Configuring Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,17 @@ Rails.application.configure do
   # Use Rack-WWWhisper for authenication in production
   #config.middleware.insert 0, "Rack::WWWhisper"
   config.action_mailer.default_url_options = { :host => 'realtortracker.herokuapp.com' }
+  # Change mail delivery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    #ActionMailer::Base.smtp_settings= {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+      :enable_starttls_auto => true
+    }
+  
 end
